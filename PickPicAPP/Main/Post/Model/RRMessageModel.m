cdc0170be3fccbadcdd975775aa28ab7ddcb45a2
MrHZD <apple@appledemacbook-pro.local>
2016-06-02 16:03:47 +0800

    黄泽东  发布图片页面

---
+++ PickPicAPP/Main/Post/Model/RRMessageModel.m
@@ -0,0 +1,13 @@
+//
+//  RRMessageModel.m
+//  PickPic
+//
+//  Created by apple on 16/6/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "RRMessageModel.h"
+
+@implementation RRMessageModel
+
+@end
---
+++ PickPicAPP/Main/Post/View/RRCustomScrollView.h
@@ -0,0 +1,13 @@
+//
+//  RRCustomScrollView.h
+//  PickPic
+//
+//  Created by apple on 16/6/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RRCustomScrollView : UIScrollView
+
+@end
